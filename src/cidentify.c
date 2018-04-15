0ae8226ab405c8abe9860f10229572c5eff6d7fe
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2018-04-15 01:07:29 -0300

    Update cidentify

--- src/cidentify.c
+++ src/cidentify.c
@@ -1,21 +1,32 @@
 #include "../include/cthread.h"
+#include "../include/cdata.h"
 
-#define GROUP_NAMES_SIZE 92
-
+/******************************************************************************
+Parâmetros:
+	name:	ponteiro para uma área de memória onde deve ser escrito um string que contém os nomes dos componentes do grupo e seus números de cartão.
+		Deve ser uma linha por componente.
+	size:	quantidade máxima de caracteres que podem ser copiados para o string de identificação dos componentes do grupo.
+Retorno:
+	Se correto => 0 (zero)
+	Se erro	   => Valor negativo.
+******************************************************************************/
 int cidentify (char *name, int size) {
+#define GROUP_NAMES_SIZE 92
+  
   char group_names[] = "Arthur L. Fuchs, 00261577 \nGabriel Martins, 00242288 \nLeonardo Marques Rodrigues, 00213751\n";
   int i = 0;
 
-  if ( size < GROUP_NAMES_SIZE ) {
-    return -1;
+  if ( !name ) {
+    return ERROR;
   }
   else {
-    while (i < size && group_names[i] != '\0') {
+    while (i < size-1 && group_names[i] != '\0') {
       name[i] = group_names[i];
       i++;
     }
 
     name[i] = '\0';
     return 0;
+
   }
 }
