0100298f9c0882acbd53e8422986e13006eb824a
gabemartins <gabrielmartins94@gmail.com>
2018-04-19 23:39:46 -0300

    cdata.c comments

--- src/cdata.c
+++ src/cdata.c
@@ -5,13 +5,12 @@
 #include "../include/support.h"
 #include "../include/queues.h"
 
-
-/* Init control variables and create main thread
-   Return 0 if susceded, -1 if not. */
+/* Inicia variáveis de controle e cria a thread main
+   Retorna 0 se conseguiu, e -1 caso contrário */
 int init_lib(void){
   TCB_t* main_thread;
 
-  /* Init control queues */
+  /* Inicia o controle das filas */
   FILA2 fila_all_thread;
   FILA2 fila_apto;
   FILA2 fila_apto_suspenso;
@@ -20,38 +19,38 @@ int init_lib(void){
   control.apto = &fila_apto;
   control.apto_suspenso = &fila_apto_suspenso;
 
-  /* Init all control queues and verify if succeded.
-     exploits short-circuit behaviour to better performance. */
+  /* Inicia as filas de controle e verifica se conseguiu
+     Explora o comportamento de curto-circuito para melhor desempenho */
   if ( CreateFila2(control.all_threads) && CreateFila2(control.apto) &&
        CreateFila2(control.apto_suspenso) ){
     return ERROR;
   }
 
-  /* Set the library as initialized */
+  /* Define a biblioteca como inicializada */
   control.init = TRUE;
-  /*Set last tid as 0 (Main Thread) */
+  /* Define o último tid como 0 (Main Thread) */
   control.last_created_tid = 0;
 
-  /* Create main thread with TID = 0 */
+  /* Cria thread principal com TID = 0*/
   main_thread = (TCB_t*)malloc(sizeof(TCB_t));
   main_thread->state = PROCST_EXEC;
   main_thread->tid = 0;
 
-  /* Insert main thread in all_threads*/
+  /* Insere thread principal em all_threads */
   AppendFila2(control.all_threads, main_thread);
 
-  /* Create end functions to threads*/
+  /* Cria funções de finalização para as threads */
   getcontext(&control.ended_thread);
   control.ended_thread.uc_link = NULL;
   control.ended_thread.uc_stack.ss_sp = (char*) malloc(STACK_SIZE);
   control.ended_thread.uc_stack.ss_size = STACK_SIZE;
-  //TODO: IMPLEMENT ENDED_THREAD
+  //TODO: IMPLEMENTAR ENDED_THREAD
   makecontext(&control.ended_thread, (void (*)(void))ended_thread, 0);
 
-  /* Set Main thread as running*/
+  /* Define Main thread como executando */
   control.running_thread = main_thread;
 
-  /* Create context to main thread*/
+  /* Cria o context para main thread */
   getcontext(&main_thread->context);
   main_thread->context.uc_link = &control.ended_thread;
   main_thread->context.uc_stack.ss_sp = (char*) malloc(STACK_SIZE);
@@ -60,55 +59,56 @@ int init_lib(void){
   return 0;
 }
 
-/* Swap threads that should execute
-   Return 0 if susceded, -1 if not. */
+/* Faz a troca para a thread que será executada
+   Retorna 0 se conseguiu, -1 caso contrário */  
 int dispatcher(){
   TCB_t* next_thread;
   TCB_t* current_thread = control.running_thread;
 
-  /* Get the first thread of FIFO */
+  /* Pega a primeira thread da FIFO */
   if (FirstFila2(control.apto))
     return ERROR;
   next_thread = GetAtIteratorFila2(control.apto);
 
-  /* No thread in able threads*/
+  /* Nenhuma thread pode ser executado*/
   if (next_thread == NULL)
     next_thread = current_thread;
   else {
-    /* Remove next thread from FIFO */
+    /* Remove a próxima thread da FIFO */
     if (DeleteAtIteratorFila2(control.apto))
       return ERROR;
-    /* Set as running_thread*/
+    /* Define como running_thread*/
     next_thread->state = PROCST_EXEC;
     control.running_thread = next_thread;
   }
 
-  /* Old running thread must be inserted in APTO queue */
+  /* Thread que estava executando precisa ser inserida na fila de APTO */
   if (current_thread->state == PROCST_EXEC){
-    /* If the next thread are the running thread do nothing*/
+    /* Se a próxima thread é a thread executando não faz nada */
     if (next_thread != current_thread){
-      /* Set state of old running thread as APTO and add to able threads*/
+      /* Define o estado da thread antiga como APTO */
       current_thread->state = PROCST_APTO;
       AppendFila2(control.apto, current_thread);
-      /* Swapping context to new thread*/
+      /* Faz o swap do context para a nova thread */
       swapcontext(&current_thread->context, &next_thread->context);
     }
   }
 
-  /* Old running thread must be blocked*/
+  /* Thread antiga precisa ser bloqueada */
   else if (current_thread->state == PROCST_BLOQ){
-    /* Swapping context to new thread*/
+    /* Faz o swap do context para a nova thread */
     swapcontext(&current_thread->context, &next_thread->context);
   }
 
-  /* Running Thread must be deleted*/
+  /* Thread executando precisa ser deletada */
   else if (current_thread->state == PROCST_TERMINO){
-    /* Remove running thread from list of all threads*/
+    /* Remove a thread executando da fila de all_threads */
     if (DeleteFromFila2(control.all_threads, current_thread))
       return ERROR;
-    /* Free TCB memory of old running thread*/
+    /* Libera o TCB da thread antigo da memoria */
     free(current_thread->context.uc_stack.ss_sp);
     free(current_thread);
+    /* Se a próxima thread é a thread executando não faz nada*/
     /* If the next thread are the running thread do nothing*/
     if (next_thread != current_thread)
       setcontext(&next_thread->context);
